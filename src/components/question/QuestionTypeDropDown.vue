e68aa303ee6fd159f20b82c5fb5f19012c76607f
Teun Spithoven <teun.spithoven@gmail.com>
2022-11-06 11:05:09 +0100

    question type dropdown bug fix

--- src/components/question/QuestionTypeDropDown.vue
+++ src/components/question/QuestionTypeDropDown.vue
@@ -5,7 +5,7 @@
   <div v-if="this.typeDropdown" id="typeDropdown">
     <ul class="buttonList">
       <li class="typeButton" v-for="t in this.types" :key="t">
-        <button @click="updateType(t)">{{ t }}</button>
+        <button v-if="t !== this.type" @click="updateType(t)">{{ t }}</button>
       </li>
     </ul>
   </div>
@@ -21,7 +21,6 @@ export default {
     return {
       typeDropdown: false,
       types: ["meerkeuze", "open", "woordwolk", "goedfout"],
-      unselectedTypes: ["meerkeuze", "open", "woordwolk", "goedfout"],
     };
   },
   emits: {
@@ -31,18 +30,6 @@ export default {
   },
   methods: {
     toggleTypeDropdown() {
-      console.log(this.types);
-      this.unselectedTypes = this.types;
-
-      var index = this.unselectedTypes
-        .map((x) => {
-          return x;
-        })
-        .indexOf(this.type);
-      if (index != -1) {
-        this.unselectedTypes.splice(index, 1);
-      }
-      index = -1;
       this.typeDropdown = !this.typeDropdown;
     },
     updateType(type) {
@@ -52,21 +39,6 @@ export default {
       this.toggleTypeDropdown();
     },
   },
-    mounted() {
-        console.log('dropdown mounted');
-    //   var index = this.types
-    //     .map((x) => {
-    //       return x;
-    //     })
-    //     .indexOf(this.type);
-
-    //   this.unselectedTypes.splice(index, 1);
-    },
-  // computed: {
-  //   unselectedTypes() {
-  //         return this.types !== this.type ? 'Yes' : 'No';
-  //   },
-  // },
 };
 </script>
 
