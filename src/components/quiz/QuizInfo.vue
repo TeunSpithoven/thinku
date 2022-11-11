1b5565045c4715476eecf4e37fbe3f20eaa65e26
Teun Spithoven <teun.spithoven@gmail.com>
2022-11-11 12:11:41 +0100

    add image while creating quiz

--- src/components/quiz/QuizInfo.vue
+++ src/components/quiz/QuizInfo.vue
@@ -1,45 +1,55 @@
 <template>
   <div id="quizInfo">
-    <div id="titel">
-      <div class="label">Titel</div>
-      <div type="text" class="inputContainer">
-        <input
-          id="titleInput"
-          class="inputs"
-          type="text"
-          v-model="this.title"
-          @change="updateQuizTitle()"
-        />
+    <div class="gridContainer">
+      <div class="info">
+        <div id="titel">
+          <div class="label">Titel</div>
+          <div type="text" class="inputContainer">
+            <input
+              id="titleInput"
+              class="inputs"
+              type="text"
+              v-model="this.title"
+              @change="updateQuizTitle()"
+            />
+          </div>
+        </div>
+
+        <div id="beschrijving">
+          <div class="label">Beschrijving</div>
+          <div type="text" class="inputContainer">
+            <textarea
+              id="descriptionInput"
+              class="inputs"
+              type="text"
+              v-model="this.description"
+              @change="updateQuizDescription()"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="image">
+        <imageUpload class="imageInput" />
+        <!-- zoek naar een giphy gif -->
+        <!-- <input type="text" id="gifInput" />
+    <div id="gifContainer"></div> -->
       </div>
     </div>
-
-    <div id="beschrijving">
-      <div class="label">Beschrijving</div>
-      <div type="text" class="inputContainer">
-        <textarea
-          id="descriptionInput"
-          class="inputs"
-          type="text"
-          v-model="this.description"
-          @change="updateQuizDescription()"
-        ></textarea>
-      </div>
-    </div>
-
-    <!-- zoek naar een giphy gif -->
-    <input type="text" id="gifInput" />
-    <div id="gifContainer"></div>
   </div>
 </template>
 
 <script>
+import imageUpload from "@/components/inputs/ImageInput.vue";
+
 export default {
   name: "QuizInfo",
+  components: {
+    imageUpload,
+  },
   data() {
     return {
       title: "",
       description: "",
-
     };
   },
   methods: {
@@ -76,7 +86,8 @@ export default {
   box-sizing: border-box;
 
   position: relative;
-  height: 293px;
+  padding: 10px;
+  /* height: 293px; */
   /* left: 178px;
     right: 177px; */
   /* top: 86px; */
@@ -84,24 +95,37 @@ export default {
   border: 1px solid #000000;
   border-radius: 10px;
 }
+.gridContainer {
+  display: grid;
+  grid-template-columns: 1.2fr 0.8fr;
+  grid-template-rows: 1fr;
+  gap: 0px 10px;
+  grid-template-areas: "info image";
+}
+.image {
+  grid-area: image;
+}
+.info {
+  grid-area: info;
+}
 .inputs {
   width: 100%;
   height: 100%;
   border-radius: 5px;
 }
 #titel {
-  position: absolute;
+  position: relative;
   height: 81px;
-  left: 45px;
+  /* left: 45px;
   right: 550px;
-  top: 33px;
+  top: 33px; */
 }
 #beschrijving {
-  position: absolute;
+  position: relative;
   height: 132px;
-  left: 45px;
+  /* left: 45px;
   right: 550px;
-  top: 122px;
+  top: 122px; */
 }
 .label {
   position: absolute;
@@ -156,6 +180,18 @@ export default {
   right: 133px;
   top: 86px;
 
+  border: 1px solid #000000;
+}
+.imageInput {
+  box-sizing: border-box;
+
+  /* position: absolute;
+  width: 100%;
+  left: 596px;
+  right: 45px;
+  top: 70px; */
+  height: 100%;
+  
   border: 1px solid #000000;
 }
 textarea {
