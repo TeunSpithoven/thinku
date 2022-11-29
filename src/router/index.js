cb3876b65281d9452eb947e0a32a98dfbdc0ec95
Teun Spithoven <teun.spithoven@gmail.com>
2022-11-29 21:13:24 +0100

    redirect base url to dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,7 @@ import DashboardView from '../views/DashboardView.vue'
 const routes = [
   {
     path: '/',
-    name: 'dashboard',
-    component: DashboardView
+    redirect: { name: 'dashboard' },
   },
   {
     path: '/dashboard',
